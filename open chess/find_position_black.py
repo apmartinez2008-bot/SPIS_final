3ec29582903c25a17c2d099eb967358973f2b833
apmartinez <apmartinez2008@gmail.com>
2026-09-02 10:25:23 -0700

    updated to work with new code

--- open chess/find_position_black.py
+++ open chess/find_position_black.py
@@ -15,69 +15,62 @@ def fen2board_black(fen_line):
                 else:
                     bool_row.append(0)
         black_player_bool_position.append(bool_row)
-    black_player_bool_position = np.array(black_player_bool_position)
-    return black_player_bool_position
+    return np.array(black_player_bool_position)
 
-def rectContains(rect,mid_point):
-    logic = rect[0]<mid_point[0]<rect[2] and rect[1]<mid_point[1]<rect[3]
-    return logic
+def point_in_quad(mid_point, quad_corners):
+    """Returns True if mid_point (x, y) is inside the 4-corner polygon."""
+    pts = np.array(quad_corners, dtype=np.int32)
+    return cv2.pointPolygonTest(pts, (float(mid_point[0]), float(mid_point[1])), False) >= 0
 
-def find_current_past_position(img_1,img_2,boxes,bool_position,FEN_line,chess_board,number_to_position_map,map_position):
-    past_bool_position = bool_position
-    current_bool_position = bool_position
-    diff_position = np.zeros((8,8),dtype=int)
+def find_current_past_position(img_1, img_2, board_squares, bool_position, FEN_line, chess_board, number_to_position_map, map_position):
     past_black_bool_position = fen2board_black(FEN_line)
+    diff_position = np.zeros((8, 8), dtype=int)
 
-    image_diff = cv2.absdiff(img_1,img_2)
-    # cv2.imshow("diff",image_diff)
-    image_diff_gray = cv2.cvtColor(image_diff,cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Gray",image_diff_gray)
-    matrix,thresold = cv2.threshold(image_diff_gray,10,255,cv2.THRESH_BINARY)
-    # cv2.imshow("thre",thresold)
-    cnts,_ = cv2.findContours(thresold, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    image_diff = cv2.absdiff(img_1, img_2)
+    image_diff_gray = cv2.cvtColor(image_diff, cv2.COLOR_BGR2GRAY)
+    _, threshold = cv2.threshold(image_diff_gray, 20, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     if len(cnts) >= 2:
-        required_contoures_mid_point = []
+        required_contours_mid_point = []
         for c in cnts:
-            area = cv2.contourArea(c)
-            if area> 500:
+            if cv2.contourArea(c) > 300:
                 (x, y, w, h) = cv2.boundingRect(c)
-                required_contoures_mid_point.append([x+int(w/2),y+int(h/2)])
-                # cv2.rectangle(diff, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                required_contours_mid_point.append([x + int(w / 2), y + int(h / 2)])
 
-        flag = np.zeros((8,8),dtype=int)
-        for i in range(8):
-                for j in range(8):
-                    for mid_point in required_contoures_mid_point:
-                        if(rectContains(boxes[i][j],mid_point)) and flag[i][j]==0:
-                            diff_position[i][j] = 2
-                            flag[i][j]=1
-
+        flag = np.zeros((8, 8), dtype=int)
         
+        # Iterates through dictionary keys (r, c) tuples instead of list indices
+        for (r, c), quad_corners in board_squares.items():
+            for mid_point in required_contours_mid_point:
+                if point_in_quad(mid_point, quad_corners) and flag[r][c] == 0:
+                    diff_position[r][c] = 2
+                    flag[r][c] = 1
+
         temp_matrix = past_black_bool_position - diff_position
-        print(temp_matrix)
         position_of_past_black = np.where(temp_matrix == -1)
         position_of_new_black = np.where(temp_matrix == -2)
 
-        player_moved = chess_board[position_of_past_black[0][0]][position_of_past_black[1][0]]
-        chess_board[position_of_past_black]=1
-        chess_board[position_of_new_black]=player_moved
+        if len(position_of_past_black[0]) == 0 or len(position_of_new_black[0]) == 0:
+            return " ", img_2, 0
 
-        move_word = number_to_position_map[int(position_of_past_black[0][0])][int(position_of_past_black[1][0])]
-        move_word+= number_to_position_map[int(position_of_new_black[0][0])][int(position_of_new_black[1][0])]
+        r1, c1 = position_of_past_black[0][0], position_of_past_black[1][0]
+        r2, c2 = position_of_new_black[0][0], position_of_new_black[1][0]
 
-        position1 = str(move_word)[0:2]
-        position2 = str(move_word)[2:4]
+        player_moved = chess_board[r1][c1]
+        chess_board[r1][c1] = "1"
+        chess_board[r2][c2] = player_moved
 
-        box_1_cordinate = map_position[position1]
-        box_2_cordinate = map_position[position2]
-        
-        position1_box = boxes[box_1_cordinate[0]][box_1_cordinate[1]]
-        position2_box = boxes[box_2_cordinate[0]][box_2_cordinate[1]]
+        move_word = number_to_position_map[r1][c1] + number_to_position_map[r2][c2]
 
         draw_img = img_2.copy()
-        cv2.rectangle(draw_img,(position1_box[0],position1_box[1]),(position1_box[2],position1_box[3]),(0,0,255),3)
-        cv2.rectangle(draw_img,(position2_box[0],position2_box[1]),(position2_box[2],position2_box[3]),(0,255,0),3)
+        
+        # Highlight source and target squares using the dictionary coordinates
+        pts1 = np.array(board_squares[(r1, c1)], np.int32).reshape((-1, 1, 2))
+        pts2 = np.array(board_squares[(r2, c2)], np.int32).reshape((-1, 1, 2))
+        cv2.polylines(draw_img, [pts1], True, (0, 0, 255), 2)
+        cv2.polylines(draw_img, [pts2], True, (0, 255, 0), 2)
 
-        return move_word,draw_img,1
+        return move_word, draw_img, 1
     else:
-        return " ",img_2,0
+        return " ", img_2, 0
